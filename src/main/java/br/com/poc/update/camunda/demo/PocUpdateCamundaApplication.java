19fbbc47ec1d0ac4e95af8b62175348fd063d1fb
Lihgp <guimaraesdpa@gmail.com>
2020-06-23 11:37:18 -0300

    boa

---
+++ src/main/java/br/com/poc/update/camunda/demo/PocUpdateCamundaApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc.update.camunda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocUpdateCamundaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocUpdateCamundaApplication.class, args);
+    }
+
+}
